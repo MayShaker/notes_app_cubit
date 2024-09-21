e6405b4396d818478ce237888977c4bea18162db
mayshaker <may.shaker@lau.edu>
2024-09-21 10:18:11 +0300

    update

--- lib/features/notes/repositories/delete_rep.dart
+++ lib/features/notes/repositories/delete_rep.dart
@@ -1,38 +1,15 @@
 
-
 import '../../../core/network/api_client.dart';
 import '../../authentication/models/response_model.dart';
 
 class DeleteNoteRepository {
-  
-  Future<ResponseModel> deleteReminder(String reminderId) async {
-    try {
-      
-      final response = await ApiClient.request(
-        url: '/reminders/$reminderId',  
-        method: 'DELETE',
-        withToken: true, 
-      );
 
-      // Check the response status code
-      if (response.statusCode == 200) {
-        return ResponseModel(
-          statusCode: 200,
-          message: 'Reminder deleted successfully',
-        );
-      } else {
-        // Return the error response from the API
-        return ResponseModel(
-          statusCode: response.statusCode,
-          message: response.message ?? 'Failed to delete the reminder',
-        );
-      }
-    } catch (e) {
-      // Handle any exceptions
-      return ResponseModel(
-        statusCode: 500,
-        message: 'An error occurred while deleting the reminder',
-      );
-    }
+  Future<ResponseModel> deleteNoteById(String noteId) async {
+    final url = '/notes/$noteId';  
+    final response = await ApiClient.request(
+      url: url,
+      method: 'DELETE',
+    );
+    return response;
   }
 }
